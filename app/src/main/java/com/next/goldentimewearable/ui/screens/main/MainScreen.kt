3eecb87904561a6003851c39f780e396c1387c04
Wesonga Bob <31077716+wesonga@users.noreply.github.com>
2023-07-05 15:24:22 +0300

    Wearos
    Wear os

---
+++ app/src/main/java/com/next/goldentimewearable/ui/screens/main/MainScreen.kt
@@ -0,0 +1,56 @@
+package com.next.goldentimewearable.ui.screens.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.wear.compose.material.Text
+import com.next.goldentimewearable.ui.components.main.Heartbeat
+
+@Composable
+fun MainScreen(model: MainViewModel = viewModel()) {
+    val bpm by model.bpm.observeAsState()
+
+    LaunchedEffect(bpm) {
+        model.sendMessage(bpm.toString())
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            model.stopWatchingBpm()
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff000000))
+            .padding(30.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Ubuntu Kwanzaa",
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            color = Color(0xffffffff)
+        )
+        Heartbeat()
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Current BPM : $bpm", fontSize = 12.sp, color = Color(0xffffffff))
+            Spacer(Modifier.height(3.dp))
+            Text(text = "You are stable", fontSize = 12.sp, color = Color(0xffffffff))
+        }
+    }
+}
